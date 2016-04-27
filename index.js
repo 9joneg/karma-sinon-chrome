8df714cb81bc5c48625bb6fdd34fbbd737c060d5
9joneg <9joneg@users.noreply.github.com>
2016-04-28 03:25:36 +0900

    add lint

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-'use strict';
+'use strict'
 
-var path = require('path');
+var path = require('path')
 
-var pattern = function(file) {
-  return { pattern: file, included: true, served: true, watched: false };
-};
+var pattern = function (file) {
+  return { pattern: file, included: true, served: true, watched: false }
+}
 
-var framework = function(files) {
-  var sinonPath = path.resolve(require.resolve('sinon'), '../../pkg/sinon.js');
-  var sinonChromePath = path.resolve(require.resolve('sinon-chrome'), '../../dist/sinon-chrome.latest.js');
-  files.unshift(pattern(sinonChromePath));
-  files.unshift(pattern(sinonPath));
-};
+var framework = function (files) {
+  var sinonPath = path.resolve(require.resolve('sinon'), '../../pkg/sinon.js')
+  var sinonChromePath = path.resolve(require.resolve('sinon-chrome'), '../../dist/sinon-chrome.latest.js')
+  files.unshift(pattern(sinonChromePath))
+  files.unshift(pattern(sinonPath))
+}
 
-framework.$inject = ['config.files'];
+framework.$inject = ['config.files']
 
 module.exports = {
   'framework:sinon-chrome': ['factory', framework]
-};
+}
