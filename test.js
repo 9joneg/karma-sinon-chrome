046a1518a929c513a037aca6f0506b34dd2005a1
9joneg <9joneg@users.noreply.github.com>
2016-04-28 04:14:28 +0900

    remove async

--- test.js
+++ test.js
@@ -3,19 +3,20 @@
 'use strict'
 
 var fs = require('fs')
-var async = require('async')
 var di = require('di')
 var framework = require('./')
 
-it('should inject files', function (done) {
+it('should inject files', function () {
   var config = { 'config': [ 'value', { files: [] } ] }
   var injector = new di.Injector([ config, framework ])
 
   injector.get('framework:sinon-chrome')
 
-  var files = injector.get('config.files')
-        .map(function (file) {
-          return file.pattern
-        })
-  async.each(files, fs.access, done)
+  injector.get('config.files')
+    .map(function (file) {
+      return file.pattern
+    })
+    .forEach(function (path) {
+      fs.accessSync(path)
+    })
 })
